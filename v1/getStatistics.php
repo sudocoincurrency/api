0d38163a68a1b244824adddf0d2c8f72612831e3
Roux <52894745+sudocode1@users.noreply.github.com>
2021-05-28 19:23:35 +0100

    add richestUser and richestUserCoins

--- v1/getStatistics.php
+++ v1/getStatistics.php
@@ -3,5 +3,7 @@ $conn = new mysqli('', '', '', '');
 $resolve = $conn->query("SELECT * FROM config WHERE id = 1");
 $res = $resolve->fetch_assoc();
 
-echo $res['id'].'|'.$res['coinsMined'].'|'.$res['coinCap'];
+$res2 = $conn->query("SELECT * FROM users ORDER BY coin DESC LIMIT 1")->fetch_assoc();
+
+echo $res['id'].'|'.$res['coinsMined'].'|'.$res['coinCap'].'|'. $res2['id'] . '|' .$res2['coin'];
 ?>
