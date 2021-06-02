cdc067bc0ab373f4a0f80ff83da914ddbfca7bc9
Roux <52894745+sudocode1@users.noreply.github.com>
2021-06-02 20:28:53 +0100

    add userCount

--- v1/getStatistics.php
+++ v1/getStatistics.php
@@ -5,5 +5,7 @@ $res = $resolve->fetch_assoc();
 
 $res2 = $conn->query("SELECT * FROM users ORDER BY coin DESC LIMIT 1")->fetch_assoc();
 
-echo $res['id'].'|'.$res['coinsMined'].'|'.$res['coinCap'].'|'. $res2['id'] . '|' .$res2['coin'];
+$res3 = $conn->query("SELECT * FROM users")->num_rows;
+
+echo $res['id'].'|'.$res['coinsMined'].'|'.$res['coinCap'].'|'. $res2['id'] . '|' .$res2['coin'] . '|' . $res3;
 ?>
